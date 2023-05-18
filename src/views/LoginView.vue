b1cfa494e74ce189b579b22885cd70d9495002e2
bumjin4u <bumjin4u@gmail.com>
2023-05-18 17:59:16 +0900

    sf

---
+++ src/views/LoginView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="userloginbox">
+      <form v-on:submit.prevent="login">
+        <input type="text" placeholder="userName" v-model="username" required>
+        <input type="password" placeholder="userPassWord" v-model="password" required>
+        <input type="submit" value="Login">
+        <p class="signup">don't have an account?
+          <a href="javascript:void(0);" @click="toggleForm">Sign up</a>
+        </p>
+      </form>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+// import {mapActios, mapGetters} from 'vuex'
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      credentials: {
+        username: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    login(event) {
+      event.preventDefault()
+      const loginITem = {
+        username : this.credentials.username,
+        password : this.credentials.password
+      }
+      if (_.trim(this.credentials.username)){
+        axios({
+          methods:'post',
+          // url: `${}`
+        })
+      }
+    },
+    signup() {
+      // Implement your signup logic here
+    },
+    toggleForm() {
+      // Implement the logic to toggle between login and signup forms here
+    }
+  }
+};
+</script>
+
+<style>
+/* .userloginbox {
+
+} */
+</style>
---
+++ src/views/SignupView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="usersignupbox">
+      <form v-on:submit.prevent="signup">
+        <h2>Create an account</h2>
+        <input type="text" placeholder="UserName" v-model="credentials.username">
+        <input type="password" placeholder="Password" v-model="credentials.password">
+        <input type="submit" value="Sign up">
+        <p>Already have an account?
+          <a href="javascript:void(0);" @click="toggleForm">Sign in</a>
+        </p>
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
